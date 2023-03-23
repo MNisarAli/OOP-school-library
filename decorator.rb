726e76b9d3b0097b8fbc8a606c67f1cb0c694860
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-03-23 15:43:28 +0500

    Implement TrimmerDecorator class

--- decorator.rb
+++ decorator.rb
@@ -16,3 +16,12 @@ class CapitalizeDecorator < BaseDecorator
     @nameable.correct_name.capitalize
   end
 end
+
+class TrimmerDecorator < BaseDecorator
+  MAX_NAME_LENGTH = 10
+
+  def correct_name
+    name = @nameable.correct_name
+    name.length > MAX_NAME_LENGTH ? name[0...MAX_NAME_LENGTH] : name
+  end
+end

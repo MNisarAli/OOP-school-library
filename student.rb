8957d87af331de20f9767b76ffa15418f42ef1da
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-03-23 13:53:19 +0500

    Add classroom as an argument of initialize method

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, parent_permission: true, name: 'Unknown')
+  def initialize(classroom, age, parent_permission: true, name: 'Unknown')
     super(age, parent_permission, name)
     @classroom = classroom
   end

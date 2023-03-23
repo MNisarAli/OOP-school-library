e4978bf32d73e958a30dd5fe64ac302dcf6349d5
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-03-23 13:53:54 +0500

    Add specialization as an argument of initialize method

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, parent_permission: true, name: 'Unknown')
+  def initialize(specialization, age, parent_permission: true, name: 'Unknown')
     super(age, parent_permission, name)
     @specialization = specialization
   end

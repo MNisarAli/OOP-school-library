6a690b34d97c0e59c13cc11f840540923b2b6e5a
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-03-24 17:26:47 +0500

    Add student to classroom upon initialization

--- student.rb
+++ student.rb
@@ -1,4 +1,5 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
   attr_accessor :classroom
@@ -6,6 +7,7 @@ class Student < Person
   def initialize(classroom, age, parent_permission: true, name: 'Unknown')
     super(age, parent_permission, name)
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hooky

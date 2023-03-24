b08e96881c14f9e3a6b205387b6657440095122f
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-03-24 17:21:02 +0500

    Initialize book & person variables

--- rental.rb
+++ rental.rb
@@ -4,4 +4,12 @@ require_relative 'person'
 class Rental
   attr_accessor :date
   attr_reader :book, :person
+
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
+    book.rentals << self
+    person.rentals << self
+  end
 end

e7e4ead6c53fe190429f0bb2fc768af0ee954154
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-03-29 23:51:20 +0500

    Refactor Teacher class initialization

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(specialization, age, parent_permission: true, name: 'Unknown')
+  def initialize(specialization, age, name, parent_permission: true)
     super(age, parent_permission: parent_permission, name: name)
     @specialization = specialization
   end

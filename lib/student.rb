99b4bd164e6812c64534b0d7936281e2a8eb2aaa
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-03-29 23:38:45 +0500

    Refactor Student class to ensure @classroom is an instance of Classroom

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,8 @@ class Student < Person
 
   def initialize(classroom, age, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
-    @classroom = classroom
-    classroom.add_student(self)
+    @classroom = classroom.is_a?(Classroom) ? classroom : Classroom.new(classroom)
+    @classroom.add_student(self)
   end
 
   def play_hooky

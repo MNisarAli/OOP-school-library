58381a58fb89a62995eabf88055da439bc9ee05b
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-03-30 13:48:32 +0500

    Create save_persons method to save @people data to json file

--- bin/preserve.rb
+++ bin/preserve.rb
@@ -1,1 +1,23 @@
 require 'json'
+
+def save_persons
+  File.open('./db/persons.json', 'w') do |file|
+    persons = @people.each_with_index.map do |person, index|
+      if person.instance_of?(Teacher)
+        { class: person.class,
+          name: person.name,
+          age: person.age,
+          specialization: person.specialization,
+          index: index }
+      else
+        { class: person.class,
+          name: person.name,
+          age: person.age,
+          classroom: person.classroom.label,
+          parent_permission: person.parent_permission,
+          index: index }
+      end
+    end
+    file.write(JSON.pretty_generate(persons))
+  end
+end
